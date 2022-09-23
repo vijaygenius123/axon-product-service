6471b7b49cd35b299ea6186e5c8bdd7a73e6cb75
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-09-23 18:56:48 +0100

    Initial commit

---
+++ src/main/java/com/vijaygenius123/axonproductservice/AxonProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.axonproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijaygenius123/axonproductservice/AxonProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.axonproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
